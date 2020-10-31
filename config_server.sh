0580150d02128298d1d56ef69508ce84e0fad763
Dinuka Jayamaha <jk.dinuka@gmail.com>
2020-10-31 13:38:50 +0000

    Terraform Workflow Commit

--- config_server.sh
+++ config_server.sh
@@ -6,4 +6,8 @@ sudo systemctl start nginx
 sudo systemctl enable nginx
 
 echo "<h1>Pomelo Production Website</h1>" >> /var/www/html/index.html
-echo "<h3>Deployed via Terraform</h3>" >> sudo tee /var/www/html/index.html
+echo "<h3>Deployed via Terraform</h3>" >> /var/www/html/index.html
+
+curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O
+chmod +x ./awslogs-agent-setup.py
+
