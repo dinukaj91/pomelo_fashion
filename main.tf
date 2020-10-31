5c3619dc9276102f1bfe7ea058910b8fead7a83e
Dinuka Jayamaha <jk.dinuka@gmail.com>
2020-10-31 13:15:32 +0000

    Terraform Workflow Commit

--- main.tf
+++ main.tf
@@ -114,21 +114,21 @@ resource "aws_security_group" "pomelo_production_generic_firewall" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "web traffic"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "ssh traffic"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
