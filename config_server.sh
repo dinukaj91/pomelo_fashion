38373092aaeb334f2aeb5ae25f606725d8f4c0c4
Dinuka Jayamaha <jk.dinuka@gmail.com>
2020-10-31 13:32:04 +0000

    Terraform Workflow Commit

--- config_server.sh
+++ config_server.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 sudo apt-get update
+
 sudo apt-get install -y nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
-echo "<h1>Pomelo Production Website</h1>" | sudo tee /var/www/html/index.html
-echo "<h2>Deployed via Terraform</h2>" | sudo tee /var/www/html/index.html
+
+echo "<h1>Pomelo Production Website</h1>" >> /var/www/html/index.html
+echo "<h3>Deployed via Terraform</h3>" >> sudo tee /var/www/html/index.html
