8f4293f2a228ac3bc2a939d439a66c2702d96b24
Dinuka Jayamaha <jk.dinuka@gmail.com>
2020-10-31 11:31:18 +0000

    Terraform Workflow Commit

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+# Specify the provider and access details
+provider "aws" {
+  region = "us-west-2"
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.101.0.0/16"
   instance_tenancy = "default"
